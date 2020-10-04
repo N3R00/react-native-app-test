45b1abd3e3d7e2dad5a231f498e7aa2da5a6b933
N3R00 <contato@nerosolutions.com.br>
2020-10-04 05:40:11 +0000

    > INTEGRADO COM AXIOS

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  state = {
+    rook: 0
+  }
+
+  findRook = async () => {
+    let response = await axios.get(`https://appsorteos.herokuapp.com/rook`);
+    this.setState({
+        rook: response.data
+    })
+  }
+
+ render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+         style={styles.button}
+         onPress={this.findRook}
+        >
+         <Text>Click me</Text>
+        </TouchableOpacity>
+        <View>
+          <Text style={styles.text}>
+            {this.state.rook._id}
+          </Text>
+          <Text style={styles.text}>
+            {this.state.rook.nome_sorteando}
+          </Text>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10
+  },
+  text: {
+      textAlign: 'center'
+  }
+})
+
+export default App;
